99846b40a77f7b432e7a30c1bef01a4096e2be9b
Jake <jakeonchain@gmail.com>
2018-12-02 21:33:26 +1100

    Add network to viewbox API

--- src/utils/loadTransactions.js
+++ src/utils/loadTransactions.js
@@ -1,9 +1,12 @@
 import xhr from 'axios';
 
 const VIEWBLOCK_URL = 'https://api.viewblock.io/zilliqa/addresshistory/';
+const VIEWBLOCK_NETWORK = 'network=testnetv3';
 
 export default async address => {
-  const { data } = await xhr.get(`${VIEWBLOCK_URL}${address}`);
+  const { data } = await xhr.get(
+    `${VIEWBLOCK_URL}${address}?${VIEWBLOCK_NETWORK}`
+  );
   if (data && data.txs) {
     return data.txs.sort((a, b) => b.timestamp - a.timestamp);
   } else {
